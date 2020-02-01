eecbaef180fa64cf24d0b5add9488b4b1bf059c4
VivekVRaga <60251730+VivekVRaga@users.noreply.github.com>
2020-02-01 17:50:02 +0200

    Add files via upload

---
+++ 8ball Simulator.py
@@ -0,0 +1,6 @@
+import random
+
+message = ['It is certain','maybe','definitly no']
+
+
+print(message[random.randint(0,len(message)-1)])
---
+++ guess the number.py
@@ -0,0 +1,24 @@
+import random
+
+def gen(guessed):
+    if int(guessed) > num:
+        print('guess is too large')
+    elif int(guessed) < num:
+        print('guess is too small')
+    else:
+        print('correct')
+
+        return 1
+
+num=random.randint(1,20)
+guessnum=0
+guessed =  input('Guess a number between 1 and 20\n')
+while 1:
+    guessnum+=1
+    if gen(guessed) == 1:
+        print('you guessed it in ',guessnum,' tries')
+        break
+    guessed = input('')
+
+
+
